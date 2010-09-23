36fa99887bf5c0d9e1197837a200c03aa59ec7ac
Gregory Crosswhite <gcross@phys.washington.edu>
2010-09-23 14:06:31 -0700

    Created initial git repository for project.

---
+++ test.py
@@ -0,0 +1,83 @@
+#@+leo-ver=4-thin
+#@+node:gcross.20100923134429.1880:@thin test.py
+#@<< Import needed modules >>
+#@+node:gcross.20100923134429.1883:<< Import needed modules >>
+import unittest
+from paycheck import *
+from numpy import array, zeros, all, double, tensordot, multiply, complex128, allclose, ones, diag, identity, dot, argmin, rank, set_printoptions
+from numpy.linalg import norm
+from numpy.random import rand
+from random import randint, choice
+import random
+import __builtin__
+from utils import *
+#@-node:gcross.20100923134429.1883:<< Import needed modules >>
+#@nl
+
+#@+others
+#@+node:gcross.20100923134429.1894:Classes
+#@+node:gcross.20100923134429.1895:TestCase
+class TestCase(unittest.TestCase):
+    #@    @+others
+    #@+node:gcross.20100923134429.1896:assertAllClose
+    def assertAllClose(self,v1,v2):
+        v1 = array(v1)
+        v2 = array(v2)
+        self.assertEqual(v1.shape,v2.shape)
+        self.assertTrue(allclose(v1,v2))
+    #@nonl
+    #@-node:gcross.20100923134429.1896:assertAllClose
+    #@+node:gcross.20100923134429.1897:assertAllEqual
+    def assertAllEqual(self,v1,v2):
+        v1 = array(v1)
+        v2 = array(v2)
+        self.assertEqual(v1.shape,v2.shape)
+        self.assertTrue(all(v1 == v2))
+    #@nonl
+    #@-node:gcross.20100923134429.1897:assertAllEqual
+    #@+node:gcross.20100923134429.1898:assertVanishing
+    def assertVanishing(self,v):
+        self.assertAlmostEqual(norm(v),0)
+    #@-node:gcross.20100923134429.1898:assertVanishing
+    #@-others
+#@-node:gcross.20100923134429.1895:TestCase
+#@-node:gcross.20100923134429.1894:Classes
+#@+node:gcross.20100923134429.1886:Tests
+#@+node:gcross.20100923134429.1888:matrix_vector_product
+class matrix_vector_product(TestCase):
+    contractor = staticmethod(
+        form_contractor([
+            ("M1","V'1"),
+            ("M2","V1"),
+        ], [
+            ("M",2),
+            ("V",1),
+        ], ("V'",1)
+        ))
+
+    @with_checker(number_of_calls=10)
+    def test(self,m=irange(1,10),n=irange(1,10)):
+        M = crand(m,n)
+        v = crand(n)
+        self.assertAllClose(dot(M,v),self.contractor(M,v))
+
+
+#@-node:gcross.20100923134429.1888:matrix_vector_product
+#@-node:gcross.20100923134429.1886:Tests
+#@-others
+
+
+set_printoptions(linewidth=132)
+
+tests = [
+    matrix_vector_product,
+    ]
+
+#@<< Runner >>
+#@+node:gcross.20100923134429.1885:<< Runner >>
+unittest.TextTestRunner(verbosity=2).run(unittest.TestSuite(map(unittest.defaultTestLoader.loadTestsFromTestCase, tests)))
+#@-node:gcross.20100923134429.1885:<< Runner >>
+#@nl
+#@nonl
+#@-node:gcross.20100923134429.1880:@thin test.py
+#@-leo
---
+++ utils.py
@@ -0,0 +1,237 @@
+#@+leo-ver=4-thin
+#@+node:gcross.20100923134429.1858:@thin utils.py
+#@@language Python
+
+#@<< Import needed modules >>
+#@+node:gcross.20100923134429.1899:<< Import needed modules >>
+import __builtin__
+from numpy import tensordot, multiply
+from numpy.random import rand
+#@-node:gcross.20100923134429.1899:<< Import needed modules >>
+#@nl
+
+#@+others
+#@+node:gcross.20100923134429.1900:Functions
+#@+node:gcross.20100923134429.1869:n2l
+#@+at
+# Utility function converting numbers to letters.
+#@-at
+#@@c
+n2l = map(chr,range(ord('A'),ord('Z')+1))
+#@-node:gcross.20100923134429.1869:n2l
+#@+node:gcross.20100923134429.1870:make_contractor
+def make_contractor(tensor_index_labels,index_join_pairs,result_index_labels,name="f"):    # pre-process parameters
+    tensor_index_labels = list(map(list,tensor_index_labels))
+    index_join_pairs = list(index_join_pairs)
+    result_index_labels = list([list(index_group) if hasattr(index_group,"__getitem__") else [index_group] for index_group in result_index_labels])
+
+    assert sum(len(index_group) for index_group in tensor_index_labels) == (sum(len(index_group) for index_group in result_index_labels)+2*len(index_join_pairs))
+
+    function_definition_statements = ["def %s(%s):" % (name,",".join(n2l[:len(tensor_index_labels)]))]
+
+    #@    << def build_statements >>
+    #@+node:gcross.20100923134429.1871:<< def build_statements >>
+    def build_statements(tensor_index_labels,index_join_pairs,result_index_labels):
+    #@+at
+    # This routine recursively builds a list of statements which performs the 
+    # full tensor contraction.
+    # 
+    # First, if there is only one tensor left, then transpose and reshape it 
+    # to match the result_index_labels.
+    #@-at
+    #@@c
+        if len(tensor_index_labels) == 1:
+            if len(result_index_labels) == 0:
+                return ["return A"]
+            else:
+                final_index_labels = tensor_index_labels[0]
+                result_indices = [[final_index_labels.index(index) for index in index_group] for index_group in result_index_labels]
+                transposed_indices = __builtin__.sum(result_indices,[])
+                assert type(transposed_indices) == list
+                assert len(final_index_labels) == len(transposed_indices)
+                new_shape = ",".join(["(%s)" % "*".join(["shape[%i]"%index for index in index_group]) for index_group in result_indices])     
+                return ["shape=A.shape","return A.transpose(%s).reshape(%s)" % (transposed_indices,new_shape)]
+    #@+at
+    # Second, if all joins have finished, then take outer products to combine 
+    # all remaining tensors into one.
+    #@-at
+    #@@c
+        elif len(index_join_pairs) == 0:
+            if tensor_index_labels[-1] is None:
+                return build_statements(tensor_index_labels[:-1],index_join_pairs,result_index_labels)
+            elif len(tensor_index_labels[-1]) == 0:
+                v = n2l[len(tensor_index_labels)-1]
+                return ["A*=%s" % v, "del %s" % v] + build_statements(tensor_index_labels[:-1],index_join_pairs,result_index_labels)
+            else:
+                v = n2l[len(tensor_index_labels)-1]
+                tensor_index_labels[0] += tensor_index_labels[-1]
+                return ["A = multiply.outer(A,%s)" % v, "del %s" % v] + build_statements(tensor_index_labels[:-1],index_join_pairs,result_index_labels)
+    #@+at
+    # Otherwise, do the first join, walking through index_join_pairs to find 
+    # any other pairs which connect the same two tensors.
+    #@-at
+    #@@c
+        else:
+            #@        << Search for all joins between these tensors >>
+            #@+node:gcross.20100923134429.1872:<< Search for all joins between these tensors >>
+            #@+at
+            # This function searches for the tensors which are joined, and 
+            # reorders the indices in the join so that the index corresponding 
+            # to the tensor appearing first in the list of tensors appears 
+            # first in the join.
+            #@-at
+            #@@c
+            def find_tensor_ids(join):
+                reordered_join = [None,None]
+                tensor_ids = [0,0]
+                join = list(join)
+                while tensor_ids[0] < len(tensor_index_labels):
+                    index_labels = tensor_index_labels[tensor_ids[0]]
+                    if index_labels is None:
+                        tensor_ids[0] += 1
+                    elif join[0] in index_labels:
+                        reordered_join[0] = index_labels.index(join[0])
+                        del join[0]
+                        break
+                    elif join[1] in index_labels:
+                        reordered_join[0] = index_labels.index(join[1])
+                        del join[1]
+                        break
+                    else:
+                        tensor_ids[0] += 1
+                assert len(join) == 1 # otherwise index was not found in any tensor
+                tensor_ids[1] = tensor_ids[0] + 1
+                while tensor_ids[1] < len(tensor_index_labels):
+                    index_labels = tensor_index_labels[tensor_ids[1]]
+                    if index_labels is None:
+                        tensor_ids[1] += 1
+                    elif join[0] in index_labels:
+                        reordered_join[reordered_join.index(None)] = index_labels.index(join[0])
+                        del join[0]
+                        break
+                    else:
+                        tensor_ids[1] += 1
+                assert len(join) == 0 # otherwise index was not found in any tensor
+                return tensor_ids, reordered_join
+
+            join_indices = [0]
+            tensor_ids,reordered_join = find_tensor_ids(index_join_pairs[0])
+
+            indices = [[],[]]
+
+            for j in xrange(2):
+                indices[j].append(reordered_join[j])
+
+            # Search for other joins between these tensors
+            for i in xrange(1,len(index_join_pairs)):
+                tensor_ids_,reordered_join = find_tensor_ids(index_join_pairs[i])
+                if tensor_ids == tensor_ids_:
+                    join_indices.append(i)
+                    for j in xrange(2):
+                        indices[j].append(reordered_join[j])
+
+            #@-node:gcross.20100923134429.1872:<< Search for all joins between these tensors >>
+            #@nl
+
+            #@        << Build tensor contraction statements >>
+            #@+node:gcross.20100923134429.1873:<< Build tensor contraction statements >>
+            tensor_vars = [n2l[id] for id in tensor_ids]
+
+            statements = [
+                "try:",
+                "   %s = tensordot(%s,%s,%s)" % (tensor_vars[0],tensor_vars[0],tensor_vars[1],indices),
+                "   del %s" % tensor_vars[1],
+                "except ValueError:",
+                "   raise ValueError('indices %%s do not match for tensor %%i, shape %%s, and tensor %%i, shape %%s.' %% (%s,%i,%s.shape,%i,%s.shape))" % (indices,tensor_ids[0],tensor_vars[0],tensor_ids[1],tensor_vars[1])
+            ]
+            #@-node:gcross.20100923134429.1873:<< Build tensor contraction statements >>
+            #@nl
+
+            #@        << Delete joins from list and update tensor specifications >>
+            #@+node:gcross.20100923134429.1874:<< Delete joins from list and update tensor specifications >>
+            join_indices.reverse()
+            for join_index in join_indices:
+                del index_join_pairs[join_index]
+
+            new_tensor_index_labels_0 = list(tensor_index_labels[tensor_ids[0]])
+            indices[0].sort(reverse=True)
+            for index in indices[0]:
+                del new_tensor_index_labels_0[index]
+
+            new_tensor_index_labels_1 = list(tensor_index_labels[tensor_ids[1]])
+            indices[1].sort(reverse=True)
+            for index in indices[1]:
+                del new_tensor_index_labels_1[index]
+
+            tensor_index_labels[tensor_ids[0]] = new_tensor_index_labels_0+new_tensor_index_labels_1
+            tensor_index_labels[tensor_ids[1]] = None
+            #@-node:gcross.20100923134429.1874:<< Delete joins from list and update tensor specifications >>
+            #@nl
+
+            return statements + build_statements(tensor_index_labels,index_join_pairs,result_index_labels)
+    #@-node:gcross.20100923134429.1871:<< def build_statements >>
+    #@nl
+
+    function_definition_statements += ["\t" + statement for statement in build_statements(tensor_index_labels,index_join_pairs,result_index_labels)]
+
+    function_definition = "\n".join(function_definition_statements)+"\n"
+
+    f_globals = {"tensordot":tensordot,"multiply":multiply}
+    f_locals = {}
+
+    exec function_definition in f_globals, f_locals
+
+    f = f_locals[name]
+    f.source = function_definition
+    return f
+#@nonl
+#@-node:gcross.20100923134429.1870:make_contractor
+#@+node:gcross.20100923134429.1875:make_contractor_from_implicit_joins
+def make_contractor_from_implicit_joins(tensor_index_labels,result_index_labels,name="f"):
+    tensor_index_labels = list(map(list,tensor_index_labels))
+    found_indices = {}
+    index_join_pairs = []
+    for i in xrange(len(tensor_index_labels)):
+        for index_position, index in enumerate(tensor_index_labels[i]):
+            if index in found_indices:
+                other_tensor = found_indices[index]
+                if other_tensor is None:
+                    raise ValueError("index label %s found in more than two tensors" % index)
+                else:
+                    # rename this instance of the index and add to the list of join pairs
+                    tensor_index_labels[i][index_position] = (i,index)
+                    index_join_pairs.append((index,(i,index)))
+                    # mark that we have found two instances of this index for
+                    # error-checking purposes
+                    found_indices[index] = None
+            else:
+                found_indices[index] = i
+    return make_contractor(tensor_index_labels,index_join_pairs,result_index_labels,name)
+#@nonl
+#@-node:gcross.20100923134429.1875:make_contractor_from_implicit_joins
+#@+node:gcross.20100923134429.1877:crand
+def crand(*shape):
+    return rand(*shape)*2-1+rand(*shape)*2j-1j
+#@-node:gcross.20100923134429.1877:crand
+#@+node:gcross.20100923134429.1879:form_contractor
+def form_contractor(edges,input_tensors,output_tensor):
+    e = {}
+
+    for index, (v1, v2) in enumerate(edges):
+        if v1 in e:
+            raise ValueError("vertex {0} appears twice".format(v1))
+        if v2 in e:
+            raise ValueError("vertex {0} appears twice".format(v2))
+        e[v1] = index
+        e[v2] = index
+
+    output_name, output_size = output_tensor
+    return make_contractor_from_implicit_joins(
+        [[e[input_name+str(index)] for index in xrange(1,input_size+1)] for (input_name,input_size) in input_tensors],
+        [e[output_name+str(index)] for index in xrange(1,output_size+1)],
+    )
+#@-node:gcross.20100923134429.1879:form_contractor
+#@-node:gcross.20100923134429.1900:Functions
+#@-others
+#@-node:gcross.20100923134429.1858:@thin utils.py
+#@-leo
